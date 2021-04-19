4c0e7b54ea8fb3063e7bcb8e1752aeee6807dfac
teor <teor@riseup.net>
2021-04-19 22:10:31 +1000

    Fix tokio 0.2 -> 0.3 runtime API

--- metrics-exporter-prometheus/src/builder.rs
+++ metrics-exporter-prometheus/src/builder.rs
@@ -129,12 +129,12 @@ impl PrometheusBuilder {
     /// installing the recorder as the global recorder.
     #[cfg(feature = "tokio-exporter")]
     pub fn install(self) -> Result<(), InstallError> {
-        let mut runtime = runtime::Builder::new()
-            .basic_scheduler()
+        let runtime = runtime::Builder::new_current_thread()
             .enable_all()
             .build()?;
 
-        let (recorder, exporter) = runtime.enter(|| self.build_with_exporter())?;
+        let _guard = runtime.enter();
+        let (recorder, exporter) = self.build_with_exporter()?;
         metrics::set_boxed_recorder(Box::new(recorder))?;
 
         thread::Builder::new()
